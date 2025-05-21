f74a47df1acb64c9956e86938e5a67d6f874325d
Dickson Ndumia <dicksonndumia19@gmail.com>
2025-05-21 15:36:01 +0300

    first commit

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -1,10 +1,20 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
 
-// Set storage destination and filename
+
+
+// Define absolute upload folder path
+const uploadPath = path.join(__dirname, "uploads", "courses");
+
+// Make sure the upload folder exists, create if missing
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/courses");
+    cb(null, uploadPath); // Absolute path here
   },
   filename: (req, file, cb) => {
     const ext = path.extname(file.originalname);
@@ -12,13 +22,13 @@ const storage = multer.diskStorage({
   },
 });
 
-// Optional: file filter to accept images and videos only
 const fileFilter = (req: any, file: Express.Multer.File, cb: any) => {
   if (file.mimetype.startsWith("image")) {
     cb(null, true);
   } else {
-    cb(new Error("Only image and  files are allowed!"), false);
+    cb(new Error("Only image files are allowed!"), false);
   }
 };
 
 export const upload = multer({ storage, fileFilter });
+
