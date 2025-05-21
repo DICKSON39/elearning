a259a32010398c58c50b3b734bd5f730ec80fca3
Dickson Ndumia <dicksonndumia19@gmail.com>
2025-05-21 15:00:08 +0300

    first commit

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -14,10 +14,10 @@ const storage = multer.diskStorage({
 
 // Optional: file filter to accept images and videos only
 const fileFilter = (req: any, file: Express.Multer.File, cb: any) => {
-  if (file.mimetype.startsWith("image/video")) {
+  if (file.mimetype.startsWith("image")) {
     cb(null, true);
   } else {
-    cb(new Error("Only image and video files are allowed!"), false);
+    cb(new Error("Only image and  files are allowed!"), false);
   }
 };
 
